a7dfaf41388ef1ca4f57f75890eae3f9dc68f898
Kevin Overall <kmoverall@gmail.com>
2015-07-15 22:28:28 -0400

    Added a slider balancer and identified problems with the physics equation for balancing: usage of angle needs to be corrected

--- Assets/Balancer.cs
+++ Assets/Balancer.cs
@@ -6,26 +6,30 @@ using System.Collections;
 //Generates a value between 0 and 1 for a Tony Hawk-style balancing mechanic
 public class Balancer : MonoBehaviour {
 
-	public float Gravity = 0.98f;
+	public float Gravity = -0.98f;
 	public float MaxValue = 1.0f;
 
 	private float angAcceleration = 0.0f;
 	private float angVelocity = 0.0f;
 	private float angle = 0.0f;
-	private float value = 0.0f;
+	protected float balanceValue = 0.0f;
 
-	void Start () {
+	protected void Start () {
 		//Initializes the angle to a random non-zero value in a narrow range centered around 0
-		while (angle == 0) {
+		/*while (angle == 0) {
 			angle = (Random.value - 0.5f) * Mathf.PI / 16;
-		}
+		}*/
+		angle = Mathf.PI / 4;
 	}
 	
-	void FixedUpdate () {
-		//Uses a physics-based balancing mechanic, based on the idea of balancing a broom on end
-		angAcceleration = (3 * Gravity * Mathf.Sin(angle)) / Mathf.Pow(MaxValue, 3);
-		angVelocity += angAcceleration;
-		angle += angVelocity;
-		value = MaxValue * Mathf.Sin(angle);
+	protected void FixedUpdate () {
+		if (balanceValue < MaxValue && balanceValue > -1 * MaxValue) {
+			Debug.Log("Update");
+			//Uses a physics-based balancing mechanic, based on the idea of balancing a broom on end
+			angAcceleration = (3 * Gravity * Mathf.Sin(angle)) / Mathf.Pow(MaxValue, 2); //angle is being used incorrectly here
+			angVelocity += angAcceleration;
+			angle += angVelocity; //which angle is this referring to? Fix
+			balanceValue = MaxValue * Mathf.Sin(angle);
+		}
 	}
 }
---
+++ Assets/SliderBalancer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+[RequireComponent (typeof(Slider))]
+public class SliderBalancer : Balancer {
+
+	Slider slider;
+
+	// Use this for initialization
+	void Start () {
+		base.Start();
+		slider = gameObject.GetComponent<Slider>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		base.FixedUpdate();
+		slider.value = (balanceValue + MaxValue) * ((slider.maxValue - slider.minValue) / 2) + slider.minValue;
+	}
+}
